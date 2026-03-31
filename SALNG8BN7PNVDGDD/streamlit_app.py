73bcc350cfb68b977ab052ffc6416fed193e7508
Carlos Richard Geraldine <carlosrichardgeraldine@outlook.com>
2026-03-31 16:56:23 +0000

    Call the API

--- SALNG8BN7PNVDGDD/streamlit_app.py
+++ SALNG8BN7PNVDGDD/streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 from snowflake.snowpark import Session
@@ -46,3 +47,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
